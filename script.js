4a8e52bce094942cd685615401b5cc202ebc587b
Gaurav Sontakke <sontakkegaurav29@gmail.com>
2024-08-07 01:00:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,35 @@ passwordField.addEventListener('input', () => {
   }
 });
 
+const generator = document.getElementById("generator-field");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const specialChar = "!@#$%^&*()_+~`|}{[]:;";
+
+const allchars = upperCase + lowerCase+numbers+specialChar;
+
+function createPassword(){
+	let password = "";
+	password += upperCase[Math.floor(Math.random() * upperCase.length)];
+	password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+	password += numbers[Math.floor(Math.random() * numbers.length)];
+	password += specialChar[Math.floor(Math.random() * specialChar.length)];
+
+	while(length>password.length){
+		password += allchars[Math.floor(Math.random() * allchars.length)];
+	}
+	generator.value = password;
+}
+
+function copypassword(){
+	generator.select();
+	document.execCommand("copy");
+}
+
+
 eyeicon.onclick = function(){
 	if(password.type == "password"){
 		password.type = "text";	
